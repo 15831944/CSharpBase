86dee6c899696161bda3be77d76aeafc5b87818f
杜鹏 <dupeng.tommy@outlook.com>
2017-04-07 18:31:54 +0800

    程序执行时间监测。

--- 控制台程序测试/Program.cs
+++ 控制台程序测试/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace 控制台程序测试
     {
         static void Main(string[] args)
         {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            int sum = 0;
+            #region 即将测试的代码段
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 10000000; j++)
+                {
+                    sum += j;
+                }
+            } 
+            #endregion
+            watch.Stop();
+            string time = watch.ElapsedMilliseconds.ToString();
+            Console.WriteLine(time);
+            Console.ReadKey();
         }
     }
 }
