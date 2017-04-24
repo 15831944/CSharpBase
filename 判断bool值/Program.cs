a09d987791d3f455866268f9faa2dcc742d8ce0c
杜鹏 <dupeng.tommy@outlook.com>
2017-04-24 17:58:29 +0800

    判断bool值

---
+++ 判断bool值/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 判断bool值
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] orgIds = { 16775, 17158 };
+            bool isXuJing = false;
+            foreach (var item in orgIds)
+            {
+                if (16775 == item)
+                {
+                    isXuJing = false;
+                }
+                break;
+            }
+            Console.WriteLine(isXuJing.ToString());
+            Console.ReadKey();
+        }
+    }
+}
