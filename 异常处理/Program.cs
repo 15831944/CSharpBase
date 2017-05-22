660364b2a99c33a5eb27f3e9bf2d6f8c5909e558
杜鹏 <dupeng.tommy@outlook.com>
2017-05-22 10:58:07 +0800

    +异常处理

---
+++ 异常处理/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 异常处理
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 1, 2, 3, 4, 5 };
+            try
+            {
+                Program.test();
+                Console.WriteLine(number[9]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+        }
+        public static void test()
+        {
+            throw new WException("发生了错误");
+        }
+
+    }
+    public class WException : Exception
+    {
+        public WException(string message) : base(message) { }
+    }
+}
