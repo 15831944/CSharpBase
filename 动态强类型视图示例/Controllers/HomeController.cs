84c3feb194f8141628a9ed25347b12746da0c42e
杜鹏 <dupeng.tommy@outlook.com>
2017-06-02 12:34:30 +0800

    +强类型视图

---
+++ 动态强类型视图示例/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 动态强类型视图示例.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class Blogs {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Blogs> list = new List<Blogs> {
+                new Blogs { Id=1,Name="蒋金楠的Blogs",Description="讲的很不错"}
+            };
+            return View(list);
+        }
+    }
+}
---
+++ 动态强类型视图示例/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="动态强类型视图示例.MvcApplication" Language="C#" %>
