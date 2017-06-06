43a53644b06ce6d7d7ab617571015580e64644f7
杜鹏 <dupeng.tommy@outlook.com>
2017-06-06 17:36:42 +0800

    +可空类型

---
+++ 可空类型/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 可空类型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = 5;
+            int? b = null;
+            Console.WriteLine("a:HasValue={0},Value={1}",a.HasValue,a.Value);
+            Console.WriteLine("b:HasValue={0},Value={1}", b.HasValue, b.GetValueOrDefault());
+            Console.ReadKey();
+        }
+    }
+}
