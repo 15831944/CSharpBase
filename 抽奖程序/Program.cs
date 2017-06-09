6c7f31b02f7f16d167bf4cd95c5a3032487e1b8b
杜鹏 <dupeng.tommy@outlook.com>
2017-06-09 18:42:04 +0800

    +抽奖程序

---
+++ 抽奖程序/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 抽奖程序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Random rd = new Random();
+            int rdNumber = rd.Next(0, 9);
+            if (rdNumber / 3 == 0)
+            {
+                Console.WriteLine("@恭喜您，中奖了！~");
+            }
+            else
+            {
+                Console.WriteLine("@很遗憾，您没有中奖。");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
