ffd6ac8d11444c61a82a71c7f7fedfdcb5e76ff3
杜鹏 <dupeng.tommy@outlook.com>
2017-06-13 11:31:19 +0800

    +字符串反序列化对象

---
+++ 序列化字符串是否包含某字符串/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace 序列化字符串是否包含某字符串
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = Console.ReadLine();
+
+            string str = "{ \"errcode\":41001,\"errmsg\":\"access_token missing hint: [eyDbQa0604vr57!]\"}";
+            var data = new { errcode = 0, errmsg = "" };
+            data = JsonConvert.DeserializeAnonymousType(str, data);
+            if (data.errcode==0)
+            {
+                Console.WriteLine("成功");
+            }
+            else
+            {
+                Console.WriteLine("失败");
+            }
+            Console.ReadKey();
+        }
+    }
+}
