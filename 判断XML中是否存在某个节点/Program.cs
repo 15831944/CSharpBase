fc490d98eaf6062e515d7b0387ec1550fe229c43
杜鹏 <dupeng.tommy@outlook.com>
2017-06-28 13:58:48 +0800

    +判断XML中是否存在某个节点

---
+++ 判断XML中是否存在某个节点/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace 判断XML中是否存在某个节点
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _xml = @"<xml>
+<AppId>第三方平台appid</AppId>
+<CreateTime>1413192760</CreateTime>
+<InfoType>unauthorized</InfoType>
+<AuthorizerAppid>公众号appid</AuthorizerAppid>
+</xml>";
+            XmlDocument doc = new XmlDocument();
+            XmlNode root;
+            XmlNodeList xnl;
+            doc.LoadXml(_xml);
+            xnl= doc.GetElementsByTagName("InfoType");
+            if (xnl.Count!=0)
+            {
+                Console.WriteLine("存在该节点。");
+            }
+            else
+            {
+                Console.WriteLine("不存在该节点。");
+            }
+        }
+    }
+}
