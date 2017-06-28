9942069668f949d97ed6013518eacb788a7e2925
杜鹏 <dupeng.tommy@outlook.com>
2017-06-28 14:10:09 +0800

    时间字符串转DateTime

---
+++ 时间字符串转DateTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 时间字符串转DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string timeString = "2017-08-09";
+            DateTime dt = DateTime.Parse(timeString);
+            Console.WriteLine(dt.ToShortDateString()); 
+        }
+    }
+}
