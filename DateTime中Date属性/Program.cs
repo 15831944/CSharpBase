cd1fcd88bc3f019aa32c1c3c6324097d5ac187c7
杜鹏 <dupeng.tommy@outlook.com>
2017-07-28 14:31:07 +0800

    *时间转换+委托

---
+++ DateTime中Date属性/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime中Date属性
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateString = "9/30/2017 10:56:54 AM";
+            DateTime date1 = DateTime.Parse(dateString,
+                System.Globalization.CultureInfo.InvariantCulture);
+            Console.WriteLine(date1.ToString(CultureInfo.InvariantCulture));
+
+            // Get date-only portion of date, without its time.
+            DateTime dateOnly = date1.Date;
+            // Display date using short date string.
+            Console.WriteLine(dateOnly);
+        }
+    }
+}
---
+++ 委托/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 委托
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello,World!~";
+            string str2 = $"好用的语法糖{str}";
+            Console.WriteLine(str2);
+        }
+    }
+}
