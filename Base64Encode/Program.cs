7b5a593c250f753fa5888a8b2633f4ca8beb7107
杜鹏 <dupeng.tommy@outlook.com>
2017-09-05 18:31:53 +0800

    jiqiren

---
+++ Base64Encode/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base64Encode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string str=  EncodeBase64(Encoding.UTF8, "我爱你中国");
+            Console.WriteLine(str);
+        }
+        public static string EncodeBase64(Encoding encode, string source)
+        {
+            string enString = "";
+            byte[] bytes = encode.GetBytes(source);
+            try
+            {
+                enString = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                enString = source;
+            }
+            return enString;
+        }
+    }
+}
