cf496ccf5641907a8eed0f74e8d7aada146daacd
杜鹏 <dupeng.tommy@outlook.com>
2018-01-02 16:04:44 +0800

    +刘老师，什么是类

---
+++ what's class/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace what_s_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Student.Amount);
+            Student t = new Student(1,"江川");
+            Console.WriteLine(Student.Amount);
+            Student t2 = new Student(2, "刘海峰");
+            Console.WriteLine(Student.Amount);
+            t.Report();
+            t2.Report();
+        }
+    }
+
+    public class Student
+    {
+        public static int Amount { get; set; }
+         static Student()
+         {
+             Amount = 100;
+         }
+        public Student(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+            Amount++;
+        }
+
+        ~Student()
+        {
+            Console.WriteLine("拜拜！~");
+        }
+         int Id { get; set; }
+         string Name { get; set; }
+
+        public void Report()
+        {
+            Console.WriteLine($"我是{Id}号学生，我的名字是{Name};");
+        }
+
+    }
+}
