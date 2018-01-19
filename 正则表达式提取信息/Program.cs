9645e81d23c1c559f984f3a7fcbed5e586d1af03
杜鹏 <dupeng.tommy@outlook.com>
2018-01-19 18:33:52 +0800

    正则表达式提取字符串

---
+++ 正则表达式提取信息/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 正则表达式提取信息
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient wc = new WebClient();
+           var data= wc.DownloadData("http://www.dianping.com/beijing/ch50/g157");
+            string pageHtml = Encoding.UTF8.GetString(data);
+            Console.WriteLine(pageHtml);
+        }
+    }
+}
