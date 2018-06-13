d8ae6e71d282824253e0c3911f1f7bc39eac8559
杜鹏 <dupeng.tommy@outlook.com>
2018-06-13 13:15:47 +0800

    时间戳

---
+++ 时间戳转换为CSharp时间/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 时间戳转换为CSharp时间
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetTime("1519452436").ToString());
+            
+        }
+        public static DateTime GetTime(string timeStamp)
+        {
+            DateTime dtStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            long lTime = long.Parse(timeStamp + "0000000");
+            TimeSpan toNow = new TimeSpan(lTime);
+            return dtStart.Add(toNow);
+        }
+    }
+}
