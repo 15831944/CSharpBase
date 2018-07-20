e3ed97507202fd7295367fe4836c2af8921d0bd9
杜鹏 <dupeng.tommy@outlook.com>
2018-07-20 09:50:47 +0800

    时间字符串数组转DateTime数组

---
+++ 时间字符串数组转换/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 时间字符串数组转换
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] times = { "2017-08-04","2015-06-06","2013-02-01"};
+            var len = times.Length;
+            DateTime[] dateTimes = new DateTime[3];
+            foreach (var item in times.ToList())
+            {
+                   dateTimes.SetValue(Convert.ToDateTime(item),times.ToList().IndexOf(item));     
+            }
+            foreach (var item in dateTimes)
+            {
+               Console.WriteLine(item); 
+            }
+           
+        }
+    }
+}
