8e0b435a0b31ef7dd720fd88cee42edfad2a0418
杜鹏 <dupeng.tommy@outlook.com>
2018-08-13 11:57:44 +0800

    UnityTest

---
+++ UnityTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace UnityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer objContainer=new UnityContainer();
+            objContainer.RegisterType<Customer>();
+            objContainer.RegisterType<IDal, OracleDal>();
+            objContainer.RegisterType<IDal,SQLServerDal>();
+     
+            Customer obj = objContainer.Resolve<Customer>();
+
+            //Customer obj = new Customer(new SQLServerDal());
+            obj.CustomerName = "test1";
+             obj.Add();
+        }
+
+
+    }
+    public class Customer
+    {
+        // private SQLServerDal Odal = new SQLServerDal();
+        //private OracleDal OdalOra = new OracleDal();
+        private IDal Odal;
+        public string CustomerName { get; set; }
+
+        public Customer(IDal iobj)
+        {
+            Odal = iobj;
+        }
+
+        public void Add()
+        {
+            //if (true)
+            //{
+            //    Odal=new SQLServerDal();
+                
+            //}
+            //else
+            //{
+            //    Odal=new OracleDal();
+                
+            //}
+            Odal.Add();
+        }
+    }
+
+    public interface IDal
+    {
+        void Add();
+    }
+
+    public class SQLServerDal:IDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("abc");
+        }
+    }
+    public class OracleDal:IDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("def");
+        }
+    }
+}
