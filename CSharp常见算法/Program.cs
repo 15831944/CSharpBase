c4232ae65e742b3132162906d828381a49d39e86
杜鹏 <dupeng.tommy@outlook.com>
2018-08-15 11:41:56 +0800

    +字符串翻转

---
+++ CSharp常见算法/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp常见算法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "I am a good man";
+            foreach (var item in s1.Reverse())
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+            string[] str = s1.Split(' ');
+            foreach (var item in str.Reverse())
+            {
+                Console.Write(item+' ');
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
